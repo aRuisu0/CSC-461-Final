766c62b275663bcd5efa2544a49b6e9a6d6e0bab
aRuisu0 <90218983+aRuisu0@users.noreply.github.com>
2022-12-12 14:23:47 -0500

    New

---
+++ speech-to-text/audio-processing/mp3_load.py
@@ -0,0 +1,19 @@
+# pip install pydub
+from pydub import AudioSegment
+
+audio = AudioSegment.from_wav("output.wav")
+# audio = AudioSegment.from_mp3("mashup.mp3")
+
+# Increase volume by 6dB
+audio = audio + 6
+
+# Repeat the clip twice
+audio = audio * 2
+
+# 2 sec fade in
+audio = audio.fade_in(2000)
+
+audio.export("mashup.mp3", format="mp3")
+
+audio2 = AudioSegment.from_mp3("mashup.mp3")
+print("Done")
